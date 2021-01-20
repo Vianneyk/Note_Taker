4b640caa2774f20558983e88ea69dd06b1e95351
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-01-20 23:17:26 +0000

    modified sementic

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const htmlRoute = require("./routes/htmlroutes");
+const apiRoutes = require("./routes/apiroutes");
 const app = express();
+
 
 const PORT = process.env.PORT || 3000
 
@@ -8,6 +10,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.use("/", htmlRoute)
+app.use("/api", apiRoutes)
+
 
 app.listen(PORT, () => {
     console.log("listening on port " + PORT);
