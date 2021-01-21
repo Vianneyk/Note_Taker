9b4d4b8e1dae5f45dcae4715491876a1e5b7510f
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-01-21 00:15:13 +0000

    formatted contain

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,11 +3,11 @@ const path = require("path");
 
 
 
-router.get("/notes", (req, res) =>{
+router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-router.get("*", (req, res) =>{
+router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
