288ca04b6b5b486c88c24ad44a12ce7f392eca35
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-01-24 20:42:28 +0000

    created a function that updates jsob file when a note is added or deleted

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -11,4 +11,10 @@ router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+function updateDb() {
+    fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+        if (err) throw err;
+        return true;
+    });
+};
 module.exports = router;
